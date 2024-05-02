3fad7c5104d6c280f58fa2958861b5a63ba911d1
Jaiditya Batra <vampconnoisseur@gmail.com>
2024-05-02 12:23:08 +0530

    Add Airbyte deployment,err: readiness & liveliness

--- main.tf
+++ main.tf
@@ -1,3 +1,41 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t2.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
@@ -23,6 +61,54 @@ provider "helm" {
       command     = "aws"
     }
   }
+}
+
+resource "aws_iam_role" "ebs_csi_driver_role" {
+  name = "eks-ebs-csi-driver-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Principal": {
+        "Federated": "${module.eks.oidc_provider_arn}"
+      },
+      "Condition": {
+        "StringEquals": {
+          "${module.eks.oidc_provider}:aud": [
+            "sts.amazonaws.com"
+          ]
+        }
+      }
+    }
+  ]
+}
+
+EOF
+
+  tags = {
+    Name = "eks-ebs-csi-driver-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
+  role       = aws_iam_role.ebs_csi_driver_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name = module.eks.cluster_name
+
+  addon_name = "aws-ebs-csi-driver"
+
+  depends_on = [
+    aws_iam_role.ebs_csi_driver_role,
+  ]
+
+  service_account_role_arn = aws_iam_role.ebs_csi_driver_role.arn
 }
 
 resource "helm_release" "airbyte" {
